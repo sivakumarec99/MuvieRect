7c3cb863997b0580f8b44f2ddee49ccc4158c139
sivakumar <sivakumarec99@gmail.com>
2023-01-19 14:48:56 +0530

    Add files via upload

---
+++ SampleMovieRect/SampleMovieRect/Constant/Constant.swift
@@ -0,0 +1,14 @@
+//
+//  Constant.swift
+//  SampleMovieRect
+//
+//  Created by Sivakumar R on 19/01/23.
+//
+
+import Foundation
+
+enum Constant {
+    enum API {
+        static let productURL = "https://fakestoreapi.com/products"
+    }
+}
---
+++ SampleMovieRect/SampleMovieRect/SampleMovieRect/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SampleMovieRect
+//
+//  Created by Sivakumar R on 19/01/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
